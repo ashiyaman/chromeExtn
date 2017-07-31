f7b365ce4d50cad714585a53de8e6e6be9c85cd9
ashiyaman <ashiya1005@gmail.com>
2017-07-31 17:17:45 +1000

    Added Time Component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import injectTapEventPlugin from 'react-tap-event-plugin';
 import Main from './imports/ui/Main';
 
-
 class App extends Component {
+
   render() {
     return (
       <div className="App">
       <MuiThemeProvider>
-
         <Main/>
       </MuiThemeProvider>
       </div>
--- src/imports/ui/Main.js
+++ src/imports/ui/Main.js
@@ -1,6 +1,7 @@
 import Unsplash from 'unsplash-js';
 import React from 'react';
 import Todo from './Todo';
+import CurrentTime from './DisplayTime';
 
 const unsplash = new Unsplash({
         applicationId: "1d5987396c54a3c2807f2c4f3a57648da2c4a70c0961df5b5d4217d9888b683f",
@@ -8,14 +9,14 @@ const unsplash = new Unsplash({
         callbackUrl : "urn:ietf:wg:oauth:2.0:oob"
     });
 
-export default class Main extends React.Component{
 
-    
+export default class Main extends React.Component{
 
 render(){
     return(
         <div>
-        <Todo/>
+          <CurrentTime />
+          <Todo/>
         </div>
     );
 }
